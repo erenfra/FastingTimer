b10736cc8b846f3d5f2c7f9e8634d71162e3c772
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-06-11 14:02:58 -0500

    Initial Commit

---
+++ FastingTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Renato Oliveira Fraga on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Renato Oliveira Fraga on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
